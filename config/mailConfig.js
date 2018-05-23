24eb40d7f855c7ccc5efa90819f456da0e1762ad
SlowSoulWen <664381601@qq.com>
2018-05-23 13:38:38 +0800

    增加博客留言邮件通知功能

---
+++ config/mailConfig.js
@@ -0,0 +1,14 @@
+/**
+ * 邮件发送配置文件
+ * QQ邮箱需要开启POP3/SMTP服务
+**/
+
+// email 登陆账号 如：xxxx@qq.com
+const emailName = '664381601@qq.com'
+// email 登陆密码
+const emailPassword = '************'
+
+module.exports = {
+	emailName,
+	emailPassword
+}
